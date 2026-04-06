fe3968fe53a77b25e31874b3d534b35cf32a6207
anilguler <anilguler035@gmail.com>
2026-04-06 15:27:18 +0300

    Update timer_anil_guler.py

--- Week06/timer_anil_guler.py
+++ Week06/timer_anil_guler.py
@@ -1,26 +1,15 @@
 import time
-from functools import wraps
 
 class Timer:
-    def __init__(self, name="Kod Bloğu"):
-        self.name = name
+    def __init__(self):
         self.start_time = None
+        self.end_time = None
 
     def __enter__(self):
         self.start_time = time.perf_counter()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        end_time = time.perf_counter()
-        execution_time = end_time - self.start_time
-        
-        print(f"--- {self.name} ---")
-        print(f"Süre: {execution_time:.6f} seconds.")
-        print("-" * (len(self.name) + 8))
-
-    def __call__(self, func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            with Timer(name=func.__name__):
-                return func(*args, **kwargs)
-        return wrapper
+        self.end_time = time.perf_counter()
+        execution_time = self.end_time - self.start_time
+        print(f"Block executed in {execution_time:.4f} seconds")
